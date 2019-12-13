a91f0e02183fd012978e61c76d1bf06eb3392df7
mestinesibov <mesti.nesibov@mail.ru>
2019-12-13 16:53:11 +0400

    news_random_api

--- home_task_10/myapp/views.py
+++ home_task_10/myapp/views.py
@@ -1,27 +1,67 @@
 from django.shortcuts import render
-# from newsapi import NewsApiClient
-# import random
+import requests
+import random
+
+
 
 # Create your views here.
+
+
+
 def owl_carousel(requests):
     return render(requests, 'owlcarousel.html')
 
 
-# newsapi = NewsApiClient(api_key='92abc5bc54b942a2902d288433122a9b')
-# newsapi = NewsApiClient(api_key='92abc5bc54b942a2902d288433122a9b')
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                     sources='bbc-news,the-verge',
-#                                     domains='bbc.co.uk,techcrunch.com',
-#                                     from_param='2019-12-12',
-#                                     to='2019-12-13',
-#                                     language='en',
-#                                     sort_by='relevancy',
-#                                     page=2)
-# def news_function(requests):
-    
-#     random_article=random.choice(sources["sources"])
-#     print(random_article)
-#     return render(requests,"news.html",{"article":random_article})
+
+
+
+def news_function(request):
+    news_url = 'https://newsapi.org/v2/top-headlines?sources=le-monde&apiKey=92abc5bc54b942a2902d288433122a9b'
+
+    r = requests.get(news_url)
+
+    total_news = r.json()['totalResults']
+
+    news_number = random.randrange(0,total_news)
+
+    title = r.json()['articles'][news_number]['title']
+    author = r.json()['articles'][news_number]['author']
+    content = r.json()['articles'][news_number]['content']
+    image = r.json()['articles'][news_number]['urlToImage']
+    published = r.json()['articles'][news_number]['publishedAt']
+
+    context = {
+        'title': title,
+        'author': author,
+        'content': content,
+        'image': image,
+        'published': published,
+    }
+
+    return render(request, 'news.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
